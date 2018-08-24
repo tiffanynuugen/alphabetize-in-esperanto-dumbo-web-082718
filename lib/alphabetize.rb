9b6a80c8d008450f1370db7d6cebcf13e6dec682
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-24 05:26:23 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
-def alphabetize(arr)
-  # code here
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+ def alphabetize(arr)
+   arr.sort_by! do |a|
+    positions = []
+    a.each_char do |char|
+      positions << ESPERANTO_ALPHABET.index(char)
+    end
+    positions
+  end
+  arr
 end
